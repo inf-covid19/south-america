5496bebc890c1f9ea267e84c57cb63ddd0518879
Francisco Bento <franciscobdsjunior@gmail.com>
2020-04-07 14:25:00 -0300

    Updates

--- polyfills.js
+++ polyfills.js
@@ -16234,8 +16234,8 @@ var __importDefault = (undefined && undefined.__importDefault) || function (mod)
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/chicobentojr/Workspace/UFRGS/covid19-inf/south-america/visualization/material-covid-br/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /home/chicobentojr/Workspace/UFRGS/covid19-inf/south-america/visualization/material-covid-br/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/chicobentojr/Workspace/UFRGS/covid19-inf/covid19-analysis/visualization/material-covid-br/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/chicobentojr/Workspace/UFRGS/covid19-inf/covid19-analysis/visualization/material-covid-br/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
